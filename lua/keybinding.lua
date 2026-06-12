63057b949688a62eb40b17f3d9b7b3b59865ac20
tangda773 <tangda773@gmail.com>
2026-06-12 21:55:47 +0900

    Update config v2026.06.12

--- lua/keybinding.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    local bufmap = function(mode, lhs, rhs, desc)
-      vim.keymap.set(mode, lhs, rhs, { desc = desc, buffer = event.buf })
-    end
-    bufmap("n", "gf", function() vim.lsp.buf.format() end, "Lsp Format")
-    bufmap("n", "gq", function() vim.diagnostic.setloclist() end, "Lsp Diagnostics Location List")
-  end,
-})
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -22,7 +22,7 @@ return {
       {"hrsh7th/cmp-nvim-lsp"},
     },
     config = function()
-      require('lsp')
+      require('lsp_util').setup_servers()
     end,
   },
 }
