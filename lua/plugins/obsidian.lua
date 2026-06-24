a32420cba5c2db30af3310b1245d39e6ee4657e6
kuangtatang <kuangtatang@tispace.com>
2026-06-24 09:53:11 +0800

    Fix Warning: 'ui.checkboxes' no longer effect the way checkboxes are ordered

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -25,10 +25,12 @@ return {
       checkboxes = {
         [" "] = { char = "☐", hl_group = "ObsidianTodo" },
         ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+        ["-"] = { char = "–", hl_group = "ObsidianCancelled" },
       },
     },
+
     checkbox = {
-      order = "default",
-    }
+      order = { " ", "x", "-" },
+    },
   },
 }
