517a54d7c127eaf0e5e413ad3c5e4c8bb31b8821
kuangtatang <kuangtatang@tispace.com>
2026-06-29 14:28:35 +0800

    feat(treesitter): configure nvim-treesitter main branch correctly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,27 @@
-  return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",          -- 用 rewrite 版，非舊 master
-    build = ":TSUpdate",
-    event = "BufReadPost",    -- 修掉你原本的 lazy = false
-    opts = {
-      ensure_installed = { "lua", "rust", "c", "vim", "vimdoc", "query" },
-    },
-    -- 不呼叫 configs.setup()，highlight/indent 交給原生
-  }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",       -- 用 rewrite 版，非舊 master
+  build = ":TSUpdate",
+  event = "BufReadPost", -- 修掉你原本的 lazy = false
+  opts = function()
+    require("nvim-treesitter").install({
+      "lua",
+      "go",
+      "rust",
+      "c",
+      "vim",
+      "vimdoc",
+      "query",
+    })
+
+    -- 啟用 TS 模組
+    return {
+      highlight = { enable = true },
+      indent    = { enable = true },
+    }
+  end,
+  config = function(_, opts)
+    -- main 分支的新 API
+    require("nvim-treesitter").setup(opts)
+  end,
+}
