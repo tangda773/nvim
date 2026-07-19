ab88bdd3ca3ed4220bc170be14734227567fad0f
tangda773 <tangda773@gmail.com>
2026-07-19 23:44:12 +0900

    feat(mini):add mini.diff

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -48,6 +48,7 @@ return {
           require("plugins.mini.extra")
           require("plugins.mini.files")
           require("plugins.mini.git")
+          require("plugins.mini.diff")
           require("plugins.mini.jump")
           -- require("plugins.mini.jump2d")
           require("plugins.mini.sessions")
