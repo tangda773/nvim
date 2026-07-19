26de60a921073ba7ea631e2080851a5ff815cd00
tangda773 <tangda773@gmail.com>
2026-07-19 23:25:15 +0900

    feat(mini):Update mini.nvim config and fix bug

--- init.lua
+++ init.lua
@@ -5,8 +5,6 @@ vim.loader.enable()
 
 require("basic")
 require("config.lazy")
-require("keybinding")
-
 
 -- ui2（0.12 新功能，cmdline / messages 重寫）
 -- vim.o.messagesopt = "hit-enter,history:500"
--- lua/plugins/mini/bufremove.lua
+++ lua/plugins/mini/bufremove.lua
@@ -1,3 +1,3 @@
 require("mini.bufremove").setup({})
-vim.keymap.set('n', 'eader>bd', MiniBufremove.delete, { desc = 'Delete buffer' })
-vim.keymap.set('n', 'eader>bD', MiniBufremove.wipeout, { desc = 'Wipeout buffer' })
+vim.keymap.set('n', '<leader>bd', MiniBufremove.delete, { desc = 'Delete buffer' })
+vim.keymap.set('n', '<leader>bD', MiniBufremove.wipeout, { desc = 'Wipeout buffer' })
