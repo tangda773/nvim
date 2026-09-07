0e4fa427966103947cc928cedb57f9902b374cb4
kuangtatang <kuangtatang@tispace.com>
2026-09-07 15:23:17 +0800

    feat(nvim): add plugin `csvview.nvim`

--- lua/plugins/csvview.lua
+++ lua/plugins/csvview.lua
@@ -33,6 +33,35 @@ return {
       jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
     },
   },
+  config = function(_, opts)
+    require("csvview").setup(opts)
+
+    local function set_csv_highlights()
+      vim.api.nvim_set_hl(0, "CsvViewHeaderLine", {
+        fg = "#cdd6f4",
+        bg = "#313244",
+        bold = true,
+      })
+
+      vim.api.nvim_set_hl(0, "CsvViewDelimiter", {
+        fg = "#585b70",
+      })
+
+      vim.api.nvim_set_hl(0, "CsvViewStickyHeaderSeparator", {
+        fg = "#6c7086",
+        bold = true,
+      })
+    end
+
+    set_csv_highlights()
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("CanLogCsvHighlights", {
+        clear = true,
+      }),
+      callback = set_csv_highlights,
+    })
+  end,
   cmd = {
     "CsvViewEnable",
     "CsvViewDisable",
