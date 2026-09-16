620d617bf87e0a5b4d25acd15f65fd5c48e7fa88
kuangtatang <kuangtatang@tispace.com>
2026-09-16 16:05:49 +0800

    feat(companion): add codecompanion extension `codecompanion-history.nvim`

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,6 +4,7 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     "lalitmee/codecompanion-spinners.nvim",
+    "ravitemer/codecompanion-history.nvim",
   },
   opts = {
     adapters = {
@@ -38,7 +39,26 @@ return {
         opts = {
           style = "snacks"
         }
-      }
+      },
+      history = {
+        enabled = true,
+        opts = {
+          auto_save = true,
+          picker = "fzf-lua",
+          summary = {
+            create_summary_keymap = "gcs",
+            browse_summary_keymap = "gbs",
+          },
+          -- memory = {
+          --   auto_create_memories_on_summmary_generation = true,
+          --   vectorcode_exe = "vectorcode",
+          --   tool_opts = {
+          --     default_num = 10, },
+          --   notify = true,
+          --   index_on_startup = false
+          -- },
+        }
+      },
     }
   },
   keys = {
